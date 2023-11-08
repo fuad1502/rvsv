4f9a756a2327be9e183c97f40bb1f3f9a8859fd4
Fuad Ismail <fuad1502@gmail.com>
2023-11-08 22:40:55 +0700

    Changed signal naming in reg_file.sv

--- systemverilog/reg_file.sv
+++ systemverilog/reg_file.sv
@@ -1,26 +1,26 @@
 module reg_file #(
     int XLEN = 32
 ) (
-    output logic [XLEN-1:0] val1,
-    output logic [XLEN-1:0] val2,
+    output logic [XLEN-1:0] valA,
+    output logic [XLEN-1:0] valB,
     input logic [4:0] rs1,
     input logic [4:0] rs2,
     input logic [4:0] rd,
-    input logic [XLEN-1:0] valR,
+    input logic [XLEN-1:0] wdata,
     input logic write_en,
     input logic clock,
     input logic reset_n
 );
   logic [XLEN-1:0] mem[32];
 
-  assign val1 = mem[rs1];
-  assign val2 = mem[rs2];
+  assign valA = mem[rs1];
+  assign valB = mem[rs2];
 
   always_ff @(posedge clock) begin : writeRegister
     if (!reset_n) begin
       mem <= '{default: 0};
     end else if (write_en) begin
-      mem[rd] <= valR;
+      mem[rd] <= wdata;
     end
   end
 
