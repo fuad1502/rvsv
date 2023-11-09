095ea951abe2993603ad12cbcbe8dcdaa051aabe
Fuad Ismail <fuad1502@gmail.com>
2023-11-09 10:15:42 +0700

    Removed reset signal from reg_file.sv and correctly handle reading from and writing to register zero

--- systemverilog/reg_file.sv
+++ systemverilog/reg_file.sv
@@ -8,18 +8,15 @@ module reg_file #(
     input logic [4:0] rd,
     input logic [XLEN-1:0] wdata,
     input logic write_en,
-    input logic clock,
-    input logic reset_n
+    input logic clock
 );
-  logic [XLEN-1:0] mem[32];
+  logic [XLEN-1:0] mem[1:32];
 
-  assign valA = mem[rs1];
-  assign valB = mem[rs2];
+  assign valA = (rs1 == 5'b00000)? 0 : mem[rs1];
+  assign valB = (rs2 == 5'b00000)? 0 : mem[rs2];
 
   always_ff @(posedge clock) begin : writeRegister
-    if (!reset_n) begin
-      mem <= '{default: 0};
-    end else if (write_en) begin
+    if (write_en && rd != 5'b00000) begin
       mem[rd] <= wdata;
     end
   end
